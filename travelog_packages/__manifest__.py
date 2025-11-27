1b4ae702ec664d4e88952801aa798119536ad264
ashir12211 <muhammedashir57@gmail.com>
2025-11-27 23:32:51 +0530

    Update __manifest__.py

--- travelog_packages/__manifest__.py
+++ travelog_packages/__manifest__.py
@@ -5,6 +5,8 @@
     'author': 'XAPP',
     'summary': 'Manage travel packages for Travelog website',
     'depends': ['base', 'website'],
+    'price': 8.00,
+    'currency': 'USD',
 
     'data': [
         'security/ir.model.access.csv',
